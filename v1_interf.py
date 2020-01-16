1150deb69d90f63e5a0652ba611328b713f03191
Drisnor <floflu19@gmail.com>
2020-01-16 15:42:53 +0100

    Commentaires divers, et taille de l'interface (non redimensionnable)

--- v1_interf.py
+++ v1_interf.py
@@ -40,6 +40,8 @@ def f_connect(name):
     res = discover_devices(lookup_names=True)
 
     for _mac, _name in res:
+        # Scanne la voiture dont le nom est la valeur du paramètre
+        # Exemple : beewi mini cooper
         if (_name.lower().startswith(name)):
             _macaddr = _mac
             set_text(en_connect, name + ' detected')
@@ -72,14 +74,20 @@ client_socket.close()
 ###########################################################################
 # Tkinter
 
+# Fenêtre principale
 win = Tk()
 win.title('Control Interface')
+win.geometry('1200x700')
+win.resizable(width=False, height=False)
 
+# Label pour afficher la liste des commandes en attente (historique)
 lb_command = LabelFrame(win, bg='blue')
-lb_command.grid(row=1, column=1, padx=5)
+lb_command.grid(row=1, column=1, padx=150, pady=200)
 sv = StringVar()
 la_hist = Label(lb_command, textvariable=sv, font='Helvetica 12 bold', fg='red', bg='purple', padx=20)
 la_hist.grid(row=1, column=1, rowspan=3)
+
+# Flèches pour contrôler
 bu_up = Button(lb_command, text='↑', command = lambda: move_forward('<Up>'))
 bu_up.grid(row=2, column=3)
 bu_left = Button(lb_command, text='←', command = lambda: move_left('<Left>'))
@@ -89,6 +97,7 @@ bu_down.grid(row=3, column=3)
 bu_right = Button(lb_command, text='→', command = lambda: move_right('<Right>'))
 bu_right.grid(row=3, column=4)
 
+# Label pour la liste des commandes
 lb_informa = LabelFrame(win, bg='red')
 lb_informa.grid(row=1, column=2, padx=5)
 la_divers = Label(lb_informa, text='bonjour', font='Helvetica 12 bold', fg='red', bg='purple', padx=20)
@@ -96,11 +105,13 @@ la_divers.grid(row=1, column=1)
 la_colist = Label(lb_informa, text='Forward ↑ Backward ↓ Left ← Right →', font='Helvetica 12 bold', fg='red', bg='purple', padx=20)
 la_colist.grid(row=2, column=1)
 
+# Label pour la connexion à une voiture avec son nom
 lb_connect = LabelFrame(win, bg='green')
 lb_connect.grid(row=2, column=1, padx=5)
 en_connect = Entry(lb_connect)
 en_connect.insert(0, 'beewi mini cooper')
 en_connect.grid(row=1, column=1, padx=5)
+# Récupère le nom de la voiture souhaitée
 bu_connect = Button(lb_connect, text='Log in', command = lambda: f_connect(en_connect.get()))
 bu_connect.grid(row=1, column=2, padx=5)
 
@@ -110,6 +121,7 @@ win.bind('<Right>', move_right)
 win.bind('<Up>', move_forward)
 win.bind('<Down>', move_backward)
 
+# affichage de l'interface
 win.mainloop()
 
 ###########################################################################
